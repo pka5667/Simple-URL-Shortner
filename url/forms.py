234967c4bb7b647adc7ccbb96a26a650d636c000
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-10-30 11:20:22 +0530

    Home Page added

--- url/forms.py
+++ url/forms.py
@@ -1,3 +1,3 @@
 from django import forms
 class Url(forms.Form):
-    url = forms.URLField(label="URL")
+    url = forms.CharField(label="URL", widget=forms.URLInput(attrs={'id': 'longurl', "placeholder":"http://site.com"}))
